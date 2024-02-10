bcb542b3f4bcb8fe8751cb63894a5e62061da7ce
Fabricio Rocha <FabricioRocha@users.noreply.github.com>
2024-02-10 17:06:59 -0300

    Basic content, some CSS

---
+++ js/book.js
@@ -0,0 +1,31 @@
+
+/*
+    Constructor function required as exercise by
+    https://www.theodinproject.com/lessons/node-path-javascript-objects-and-object-constructors
+*/
+function Book (title, author, noPages, isRead) {
+    this.title = title;
+    this.author = author || "unknown author";
+    this.noPages = noPages || 0;
+    this.isRead = isRead;
+}
+
+Book.prototype.info = function() {
+    let status = (this.isRead == true) ? "already read" : "not read yet";
+    return `${this.title} by ${this.author}, ${this.noPages} pages, ${status}`;
+}
+
+Book.prototype.toggleRead = function() {
+    this.isRead = !this.isRead;
+}
+
+const arrLib = [];
+
+function bookAdd (title, author, noPages, isRead) {
+    let newbook = new Book(title, author, noPages, isRead);
+    arrLib.push(newbook);
+}
+
+function bookRemoveByIndex (idx) {
+    arrLib.splice(idx, 1);
+}
