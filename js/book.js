ad7711c9a42397ca28b7c7e6d726006b1149154f
Fabricio Rocha <19875696+FabricioRocha@users.noreply.github.com>
2024-02-13 14:21:13 -0300

    Changed insertion/removal from index to id

--- js/book.js
+++ js/book.js
@@ -3,28 +3,29 @@
     Constructor function required as exercise by
     https://www.theodinproject.com/lessons/node-path-javascript-objects-and-object-constructors
 */
+
+var bookCount = 0;
+const arrLib = [];
 function Book (title, author, noPages, isRead) {
     this.title = title;
     this.author = author || "unknown author";
     this.noPages = noPages || 0;
     this.isRead = isRead;
-}
-
-Book.prototype.info = function() {
-    let status = (this.isRead == true) ? "already read" : "not read yet";
-    return `${this.title} by ${this.author}, ${this.noPages} pages, ${status}`;
+    this.id = bookCount++;
 }
 
 Book.prototype.toggleRead = function() {
     this.isRead = !this.isRead;
 }
 
-const arrLib = [];
-
 function bookAdd (title, author, noPages, isRead) {
     let newbook = new Book(title, author, noPages, isRead);
+    arrLib.push(newbook);
+    return newbook.id;
+    /*
     let index = arrLib.push(newbook) - 1;
     return index;
+    */
 }
 
 function bookRemoveByIndex (idx) {
@@ -33,4 +34,18 @@ function bookRemoveByIndex (idx) {
 
 function bookGetByIndex (idx) {
     return arrLib[idx];
+}
+
+function bookGetById (id) {
+    let foundAt = arrLib.findIndex(
+        (testeditem)=> testeditem.id === id
+    );
+    return bookGetByIndex(foundAt);
+}
+
+function bookRemoveById(id) {
+    let foundAt = arrLib.findIndex(
+        (testeditem, arrindex, arrLib)=> testeditem.id === id
+    );
+    bookRemoveByIndex(foundAt);
 }
